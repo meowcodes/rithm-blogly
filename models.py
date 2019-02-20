a926a362a2833c5882835d89f53d7098b3ea9c6d
meowcodes <meowcodes@gmail.com>
2019-02-20 13:48:45 -0800

    create user model

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://www3.nd.edu/~streslab/assets/img/ra_pics/Placeholder_Photo.png"
+
+def connect_db(app):
+    """ Connects to Database"""
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """ Users """"
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                    primary_key = True,
+                    autoincrement = True)
+    first_name = db.Column(db.String(50),
+                            nullable= False)
+    last_name = db.Column(db.String(50),
+                            nullable= False)
+    image_url = db.Column(db.Text,
+                            default= DEFAULT_IMG_URL)
+    
